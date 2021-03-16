e2de6d96c5f263ad9957de1fabc9625617204ec6
Irfan Nugraha <irfannugraha844@gmail.com>
2021-03-16 13:44:47 +0700

    Create model user.php

---
+++ app/Models/user.php
@@ -0,0 +1,11 @@
+<?php namespace App\Models;
+
+use CodeIgniter\Model;
+
+class user extends BaseModel
+{
+    protected $table      = 'user'; // nama tabel yang terhubung
+    protected $primaryKey = 'id'; // primary key tabel
+    protected $returnType = 'array'; 
+    protected $allowedFields= ['id','uniqueCode','nama','email','token','password','kota','deleteDate']; // kolom yang bisa dilakukan input, update, delete
+}
